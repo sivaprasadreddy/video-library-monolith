36055bea44d2dc4adb215f45d798f0a9e890fcf2
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-08-05 18:17:24 +0530

    Upgrading SpringSecurity config

--- video-library/src/main/java/com/sivalabs/videolibrary/config/security/WebSecurityConfig.java
+++ video-library/src/main/java/com/sivalabs/videolibrary/config/security/WebSecurityConfig.java
@@ -1,80 +1,45 @@
 package com.sivalabs.videolibrary.config.security;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, proxyTargetClass = true)
-@RequiredArgsConstructor
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().disable();
+        http.headers().frameOptions().sameOrigin();
+        http.authorizeRequests()
+                .antMatchers(
+                        "/webjars/**",
+                        "/resources/**",
+                        "/static/**",
+                        "/js/**",
+                        "/css/**",
+                        "/images/**",
+                        "/favicon.ico",
+                        "/h2-console/**")
+                .permitAll()
+                .antMatchers("/", "/login", "/registration")
+                .permitAll()
+                .antMatchers("/h2-console/**")
+                .permitAll();
 
-    private final PasswordEncoder passwordEncoder;
+        http.formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error")
+                .permitAll();
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
+        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
 
-    @Configuration
-    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
-        @Override
-        public void configure(WebSecurity web) {
-            web.ignoring()
-                    .antMatchers(
-                            "/static/**",
-                            "/js/**",
-                            "/css/**",
-                            "/images/**",
-                            "/favicon.ico",
-                            "/h2-console/**");
-        }
-
-        @Bean
-        @Override
-        public AuthenticationManager authenticationManagerBean() throws Exception {
-            return super.authenticationManagerBean();
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.csrf()
-                    .disable()
-                    .authorizeRequests()
-                    .antMatchers("/resources/**", "/webjars/**")
-                    .permitAll()
-                    .antMatchers("/registration", "/forgot-password", "/reset-password")
-                    .permitAll()
-                    .antMatchers("/h2-console/**")
-                    .permitAll()
-                    .anyRequest()
-                    .permitAll()
-                    // .anyRequest().authenticated()
-                    .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/")
-                    .failureUrl("/login?error")
-                    .permitAll()
-                    .and()
-                    .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .permitAll();
-        }
+        return http.build();
     }
 }
--- video-library/src/test/java/com/sivalabs/videolibrary/common/AbstractMvcUnitTest.java
+++ video-library/src/test/java/com/sivalabs/videolibrary/common/AbstractMvcUnitTest.java
@@ -3,6 +3,7 @@ package com.sivalabs.videolibrary.common;
 import static com.sivalabs.videolibrary.common.TestConstants.PROFILE_TEST;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sivalabs.videolibrary.config.security.WebSecurityConfig;
 import com.sivalabs.videolibrary.customers.domain.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,7 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles(PROFILE_TEST)
-@Import({TestConfig.class})
+@Import(WebSecurityConfig.class)
 public abstract class AbstractMvcUnitTest {
 
     @Autowired protected MockMvc mockMvc;
