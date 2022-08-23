ef143944aa7c28471070e388ac69147857b9018c
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-08-23 09:33:14 +0530

    Upgrading dependencies

---
+++ cypress-e2e-tests/cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+require('dotenv').config()
+
+module.exports = defineConfig({
+
+  env: {
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress-e2e-tests/cypress/e2e/1-video-library/browse-catalog.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('video-library', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8080')
+
+    })
+
+    it('displays movies list', () => {
+        //cy.get('.product').should('exist')
+        //cy.get('.product').should('have.length', 24)
+    })
+})
